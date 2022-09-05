5f5054dd967d3e721c0b5fffe372e1ff7519172f
DylanMerlateau <dylan.merlateau@gmail.com>
2022-09-05 11:01:55 +0200

    modif 2

--- src/main/java/com/inti/entities/Utilisateur.java
+++ src/main/java/com/inti/entities/Utilisateur.java
@@ -1,5 +1,99 @@
 package com.inti.entities;
 
-public class Utilisateur {
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Utilisateur implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nom;
+	private String prenom;
+	@Temporal(TemporalType.DATE)
+	private Date dateNaissance;
+	private int age;
+	private long cin;
+	private long tel;
+	private String numPassport;
+	public Utilisateur() {
+		super();
+
+	}
+	public Utilisateur(Long id, String nom, String prenom, Date dateNaissance, int age, long cin, long tel,
+			String numPassport) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.dateNaissance = dateNaissance;
+		this.age = age;
+		this.cin = cin;
+		this.tel = tel;
+		this.numPassport = numPassport;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public long getCin() {
+		return cin;
+	}
+	public void setCin(long cin) {
+		this.cin = cin;
+	}
+	public long getTel() {
+		return tel;
+	}
+	public void setTel(long tel) {
+		this.tel = tel;
+	}
+	public String getNumPassport() {
+		return numPassport;
+	}
+	public void setNumPassport(String numPassport) {
+		this.numPassport = numPassport;
+	}
+	@Override
+	public String toString() {
+		return "Utilisateur [id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", dateNaissance=" + dateNaissance
+				+ ", age=" + age + ", cin=" + cin + ", tel=" + tel + ", numPassport=" + numPassport + "]";
+	}
+	
+	
 
 }
